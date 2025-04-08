985ccbf82a86646764d0f5c051511938b540d20a
Venus <venusyip@outlook.com>
2025-04-08 08:05:54 +0100

    Add link to article section

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -8,7 +8,11 @@ export default function Article() {
       />
       <p className='card__tag'>Learning</p>
       <p className='card__date'>Published 21 Dec 2023</p>
-      <h1 className='card__title'>HTML & CSS foundations</h1>
+      <h1 className='card__title'>
+        <a href='' className='card__link'>
+          HTML & CSS foundations
+        </a>
+      </h1>
       <p className='card__description'>
         These languages are the backbone of every website, defining structure,
         content, and presentation.
