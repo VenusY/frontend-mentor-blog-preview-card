c772664da5b1aded6d87038d09158280b5f6874b
Venus <venusyip@outlook.com>
2025-04-08 05:07:00 +0100

    Update Webpack config to handle .webp files

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -25,7 +25,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|svg)$/i,
+        test: /\.(png|jpe?g|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'assets/[hash][ext]',
